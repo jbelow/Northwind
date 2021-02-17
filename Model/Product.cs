e619dda4964987808de0262048d488179575ad7d
Joshua Below <33503583+jbelow@users.noreply.github.com>
2021-02-17 16:35:43 -0600

    fixed everything up to migration
    - dotnet add package Microsoft.EntityFrameworkCore.Tools -v 3.1.11
    --------------------------------------
    - dotnet tool install -g dotnet-ef
    - dotnet tool update -g dotnet-ef
    --------------------------------------
    - dotnet ef migrations add CreateDatabase
    - dotnet ef database update

--- Model/Product.cs
+++ Model/Product.cs
@@ -10,12 +10,12 @@ namespace Northwind.Model
         public int ProductId { get; set; }
         public string ProductName { get; set; }
         public string QuantityPerUnit { get; set; }
-        public decimal? UnitPrice { get; set; }
-        public short? UnitsInStock { get; set; }
-        public short? UnitsOnOrder { get; set; }
-        public short? ReorderLevel { get; set; }
+        public decimal UnitPrice { get; set; }
+        public short UnitsInStock { get; set; }
+        public short UnitsOnOrder { get; set; }
+        public short ReorderLevel { get; set; }
         public bool Discontinued { get; set; }
-        public int? CategoryId { get; set; }
+        public int CategoryId { get; set; }
         public virtual Category Categories { get; set; }
 
     }
