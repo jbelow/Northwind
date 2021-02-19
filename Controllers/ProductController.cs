a6ed2839cbf71abbab5442c9f076d4c05a121939
Joshua Below <33503583+jbelow@users.noreply.github.com>
2021-02-19 15:22:31 -0600

    got the query to work and how I want to display it

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -3,12 +3,15 @@ using Microsoft.AspNetCore.Mvc;
 using Northwind.Model;
 using System.Linq;
 
+
 namespace Northwind.Controllers
 {
     public class ProductController : Controller
     {
+        private NorthwindContext _northwindContext;
+        public ProductController(NorthwindContext db) => _northwindContext = db;
         
-        
-        public IActionResult Category() => View();
+        //this is the part that says "hey here is what you are routing to" and sends the database info along with it
+        public IActionResult Category() => View(_northwindContext.Categories.OrderBy(c => c.CategoryName));
     }
 }
