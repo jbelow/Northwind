83c46e56aead482143c4b6dd6562a194838a7e01
Joshua Below <33503583+jbelow@users.noreply.github.com>
2021-03-04 16:17:39 -0600

    cleaned up my code and finished the toast

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -14,12 +14,7 @@ namespace Northwind.Controllers
         //this is the part that says "hey here is what you are routing to" and sends the database info along with it
         public IActionResult Category() => View(_northwindContext.Categories.OrderBy(c => c.CategoryName));
 
-        public IActionResult ProductDisplayList(int id) => View(new ViewProducts
-        {
-            category = _northwindContext.Categories.FirstOrDefault(c => c.CategoryId == id),
-            products = _northwindContext.Products.Where(p => (p.CategoryId == id) &&
-					                                         (p.Discontinued == false))
-        });
+        public IActionResult ProductDisplayList(int id) => View(_northwindContext.Products.Where(p => p.CategoryId == id && p.Discontinued == false).OrderBy(p => p.ProductName));
 
     }
 }
--- wwwroot/js/client.js
+++ wwwroot/js/client.js
@@ -4,30 +4,12 @@ $(function () {
     $('.code').on('click', function (e) {
         
         e.preventDefault();
-        // console.log("Test")
 
-        // //setting the data in the toast
-        // switch ($('.code').index(this)) {
-        //     case 0:
-        //         console.log("this is the first one");
-        //         // $("div").data("name", "Tis the seasoning");
-        //         $("div").data("code", "3451");
-        //         break;
-        //     case 1:
-        //         // $("div").data("name", "Go Green");
-        //         $("div").data("code", "3461");
-        //         break;
-        //     case 2:
-        //         // $("div").data("name", "Half Price");
-        //         $("div").data("code", "2345");
-        //         break;
-        // }
-        // productName = $("div").data("name");
-        // discountCode = $("div").data("code");
-        // $("#product").text("" + productName);
-        // $("#code").text("" + discountCode);
+        const discountCode = $("#" + e.target.id).attr("data-code");
+        const productName = $("#" + e.target.id).attr("data-product");
 
-        
+        $("#product").text("" + productName);
+        $("#code").text("" + discountCode);
 
         // first pause the audio (in case it is still playing)
         toast.pause();
@@ -39,7 +21,6 @@ $(function () {
     });
     
 });
-
 
 $(document).keydown(function (e) {
     // ESCAPE key pressed
