83673e0298200fa2bf252d3e4478c95cb7ac938b
Joshua Below <33503583+jbelow@users.noreply.github.com>
2021-03-04 11:43:17 -0600

    adding the toast

--- wwwroot/js/client.js
+++ wwwroot/js/client.js
@@ -1,3 +1,50 @@
-$(function(){
-    // alert("Hello World");
+$(function () {
+    var toast = new Audio('media/toast.wav');
+
+    $('.code').on('click', function (e) {
+        
+        e.preventDefault();
+        // console.log("Test")
+
+        // //setting the data in the toast
+        // switch ($('.code').index(this)) {
+        //     case 0:
+        //         console.log("this is the first one");
+        //         // $("div").data("name", "Tis the seasoning");
+        //         $("div").data("code", "3451");
+        //         break;
+        //     case 1:
+        //         // $("div").data("name", "Go Green");
+        //         $("div").data("code", "3461");
+        //         break;
+        //     case 2:
+        //         // $("div").data("name", "Half Price");
+        //         $("div").data("code", "2345");
+        //         break;
+        // }
+        // productName = $("div").data("name");
+        // discountCode = $("div").data("code");
+        // $("#product").text("" + productName);
+        // $("#code").text("" + discountCode);
+
+        
+
+        // first pause the audio (in case it is still playing)
+        toast.pause();
+        // reset the audio
+        toast.currentTime = 0;
+        // play audio
+        toast.play();
+        $('#toast').toast({ autohide: false }).toast('show');
+    });
+    
+});
+
+
+$(document).keydown(function (e) {
+    // ESCAPE key pressed
+    if (e.keyCode == 27) {
+        // $(toast).delete(); // this just makes it so that it can never come back
+        document.getElementById("closeToast").click();
+    }
 });
