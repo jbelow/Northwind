daff287d896d35701f7099e708c089493b15837f
Joshua Below <33503583+jbelow@users.noreply.github.com>
2021-03-04 16:49:58 -0600

    added the discounts page

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -15,6 +15,7 @@ namespace Northwind.Controllers
         public IActionResult Category() => View(_northwindContext.Categories.OrderBy(c => c.CategoryName));
 
         public IActionResult ProductDisplayList(int id) => View(_northwindContext.Products.Where(p => p.CategoryId == id && p.Discontinued == false).OrderBy(p => p.ProductName));
+        public IActionResult Discount() => View(_northwindContext.Discounts.Where(d => d.StartTime <= DateTime.Now && d.EndTime > DateTime.Now));
 
     }
 }
